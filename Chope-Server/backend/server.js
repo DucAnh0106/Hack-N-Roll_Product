09f8a30f7b1b7ab82a5038eb3ecc82e59d2d55a3
Jonathan Tan <jonathantan@Jonathans-MacBook-Air.local>
2026-01-17 13:33:08 +0800

    backend functionalities

---
+++ Chope-Server/backend/server.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io');
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.json());
+
+let dashboardSocket = null;
+let espSocket = null;
+
+let lifted = false;
+
+app.post('/alert', (req, res) => {
+    lifted = true;
+    console.log('Alert triggered');
+    if (dashboardSocket) {
+        dashboardSocket.emit('alert', { message: "TRESPASSER DETECTED!" });
+    }
+    res.json({ ok: true });
+})
+
+app.post('/roast', (req, res) => {
+    const { roastType } = req.body;
+    console.log('Selected roast: ' + roastType);
+    if (espSocket) {
+        espSocket.emit('roast', { roastType });
+    }
+    res.json({ ok: true });
+})
+
+
+io.on('connection', (socket) => {
+    console.log("A client connected!");
+
+    const clientType = socket.handshake.query.type;
+    console.log('Client type:', clientType);
+
+    if (clientType === 'dashboard') {
+        if (dashboardSocket) {
+            socket.disconnect();
+            console.log('Dashboard already connected. Rejecting new connection.');
+            return;
+        }
+        dashboardSocket = socket;
+        console.log('Dashboard connected.');
+        socket.emit('status', { lifted }); 
+    } else if (clientType === 'esp') {
+        if (espSocket) {
+            socket.disconnect();
+            console.log('ESP already connected. Rejecting new connection.');
+            return;
+        }
+        espSocket = socket;
+        console.log('ESP connected.');
+    }
+
+    socket.on('roast', (data) => {
+        console.log('Selected roast:', data.roastType);
+        if (espSocket) {
+            espSocket.emit('roast', { roastType: data.roastType });
+        }
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', clientType);
+        if (clientType === 'dashboard') {
+            dashboardSocket = null;
+        } else if (clientType === 'esp') {
+            espSocket = null;
+        }
+    })
+})
+
+server.listen(3000, () =>
+  console.log("Backend running on http://localhost:3000")
+);
